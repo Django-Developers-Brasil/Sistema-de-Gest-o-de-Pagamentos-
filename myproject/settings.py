6ca34d19192b2ae9385e8b42eca6c772e5e2a458
NathanOnCodes <66134107+NathanOnCodes@users.noreply.github.com>
2024-09-05 17:20:35 -0300

    fix: alterando a variavel DEBUG

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4p))wupzjg0p00fb#o3zs&kvw*%xqm(n38^ev7ilt43#i5qfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
