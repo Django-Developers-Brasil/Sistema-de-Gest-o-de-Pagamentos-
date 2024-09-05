7461ddc018a25a47ecb048bbd2acc662a41a0780
NathanOnCodes <contatonathancf@gmail.com>
2024-09-05 17:09:23 -0300

    feat: adicionando pagina 404 customizada #38

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4p))wupzjg0p00fb#o3zs&kvw*%xqm(n38^ev7ilt43#i5qfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
