71ffaf0ab86872b6af88c6f4aa8d30792215a678
thiago-abc <thiagoabc@gmail.com>
2024-09-10 17:53:51 -0300

    feature: remover a opção de delete do django admin #19

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import CarServ
 
-admin.site.register(CarServ)
+
+class CarServAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        # Retorne False para desabilitar a opção de deletar
+        return False
+    
+admin.site.register(CarServ, CarServAdmin)
