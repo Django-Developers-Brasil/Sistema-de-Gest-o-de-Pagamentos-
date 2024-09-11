61d53ce732670e502f74b03f3f88b656351747a5
Paulo Andrade <eupauloandrade@gmail.com>
2024-09-10 23:15:05 -0300

    feat: template dinamico com dados do banco WIP #12

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import index, Error
 
 urlpatterns = [
     path('', index,name='index'),
     path('404/', Error, name='404'),
-]
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Adiciona a configuração para servir arquivos de mídia durante o desenvolvimento
 
 
 
