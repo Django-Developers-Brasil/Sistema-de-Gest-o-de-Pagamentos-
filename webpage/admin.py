b9e78b22448eaf554c961322232a226bab1bd885
Paulo Andrade <eupauloandrade@gmail.com>
2024-09-12 00:34:54 -0300

    feature: added action export as csv #22

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
-from django.http import HttpRequest
 from .models import CarServ
+import csv
+from django.http import HttpResponse
 
-class CarServAdmin(admin.ModelAdmin):
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta.model_name)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Exportar Selecionados"
+
+
+@admin.register(CarServ)
+class CarServAdmin(admin.ModelAdmin, ExportCsvMixin):
     def has_delete_permission(self, request, obj=None):
         return False
 
     def has_add_permission(self, request):
         return False
-    
-    
-admin.site.register(CarServ, CarServAdmin)
+
+    actions = ["export_as_csv"]
