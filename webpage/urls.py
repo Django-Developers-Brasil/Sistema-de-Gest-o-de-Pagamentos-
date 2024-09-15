32cea3762e1be9f1cc0ddc483eefa3cabc21fc21
Paulo Andrade <eupauloandrade@gmail.com>
2024-09-15 12:29:00 -0300

    feat: formulario de contato + rota completa e links #104 e #106

--- webpage/urls.py
+++ webpage/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-from .views import index, Error
+from .views import index, Error, Contact
 
 urlpatterns = [
     path('', index,name='index'),
     path('404/', Error, name='404'),
+    path('contact/', Contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Adiciona a configuração para servir arquivos de mídia durante o desenvolvimento
 
