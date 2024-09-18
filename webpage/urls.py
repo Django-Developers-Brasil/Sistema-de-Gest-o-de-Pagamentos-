e5d29149984ca3f26a49447e2f0d16979a0a8cda
Paulo Andrade <eupauloandrade@gmail.com>
2024-09-18 18:04:41 -0300

    built: api endpoint for webpage carserv model #160

--- webpage/urls.py
+++ webpage/urls.py
@@ -2,14 +2,32 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import Index, Error, Contact, NewsLetter
+from ninja import NinjaAPI
+from .api import carserv_router
+
+
+# Criando a instância da API
+api = NinjaAPI()
+
+# Registrando os routers
+api.add_router("/v1/", carserv_router)
+
 
 urlpatterns = [
     path('', Index,name='index'),
     path('404/', Error, name='404'),
     path('contact/', Contact, name='contact'),
     path('newsletter/', NewsLetter, name='newsletter'),
+    path("api/", api.urls), # http://127.0.0.1:8000/api/v1/webpage/get/all
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Adiciona a configuração para servir arquivos de mídia durante o desenvolvimento
+
+
+
+
+
+
 
 
 
