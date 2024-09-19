4a7efe6629fcdd70c322a7d153513738fd4f1214
Paulo Andrade <eupauloandrade@gmail.com>
2024-09-18 21:48:12 -0300

    stage commit before pull

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,7 +22,7 @@ from webpage.views import index, Error
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('c-panel/', admin.site.urls),
     path('', include('webpage.urls')),    
 ]
 
